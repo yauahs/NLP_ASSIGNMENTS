0814189a06b8ed1571bd0f51a1f0f93970a3d3b8
Tejas Gunjal <92801330+yauahs@users.noreply.github.com>
2023-09-20 14:21:38 +0530

    Add files via upload

---
+++ assign -02.py
@@ -0,0 +1,13 @@
+import gensim
+from gensim import corpora
+
+text1 = ["""I want to go to school . I am going to school now ."""]
+
+tokens1 = [[item for item in line.split()] for line in text1]
+g_dict1 = corpora.Dictionary(tokens1)
+
+print("The dictionary has: " +str(len(g_dict1)) + " tokens\n")
+print(g_dict1.token2id)
+
+g_bow =[g_dict1.doc2bow(token, allow_update = True) for token in tokens1]
+print("Bag of Words : ", g_bow)
